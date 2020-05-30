e15fb1f582f4aecf193bbd8b7386cadaed7793cc
Mikhil Desai <mikhildesai@gmail.com>
2020-05-30 18:57:12 +0530

    Calculating Wages for a Month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,26 +2,34 @@ class EmpWageComputation{
 	public static void main(String[] args){
 
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empcheck = (int)(Math.random()*10)%3;
-		//System.out.println(empcheck);
 
-		int wage_per_hour = 20 , wage=0 , emphrs=0;
+		int num_working_days=20, totalwage=0;
 
-		switch (empcheck)
+		for (int day=0; day < num_working_days ; day++)
 		{
-			case 1 :  // FullTime
-				emphrs = 8;
-				break;
-			case 2 :  // PartTime
-   			emphrs = 4;
-				break;
-			default :					// Absent
-				emphrs = 0;
+			int empcheck = (int)(Math.random()*10)%3;
+			//System.out.println(empcheck);
 
+			int wage_per_hour = 20 , wage=0 , emphrs=0;
+
+			switch (empcheck)
+			{
+				case 1 :  // FullTime
+					emphrs = 8;
+					break;
+				case 2 :  // PartTime
+   				emphrs = 4;
+					break;
+				default :					// Absent
+					emphrs = 0;
+
+			}
+
+				wage = wage_per_hour*emphrs;
+				System.out.println("Employee wage="+wage);
+				totalwage += wage;
 		}
-
-			wage = wage_per_hour*emphrs;
-			System.out.println("Employee wage="+wage);
+			System.out.println("Monthly wage is="+totalwage);
 	}
 
 
