24a2e3bcff5777441b140b7cd88bd3feb73c9a98
Mikhil Desai <mikhildesai@gmail.com>
2020-05-30 18:13:14 +0530

    Employee attendance calculation using random function

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,13 @@
 class EmpWageComputation{
 	public static void main(String[] args){
+
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int empcheck = (int)(Math.random()*10)%2;
+
+		if (empcheck == 1)
+			System.out.println("present");
+		else
+   		System.out.println("absent");
 	}
 
 
