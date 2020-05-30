39fd00209cf9f38e76f16072ca56192113f845d5
Mikhil Desai <mikhildesai@gmail.com>
2020-05-30 18:35:01 +0530

    Added part time employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,17 +2,20 @@ class EmpWageComputation{
 	public static void main(String[] args){
 
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empcheck = (int)(Math.random()*10)%2;
+		int empcheck = (int)(Math.random()*10)%3;
+		//System.out.println(empcheck);
 
 		int wage_per_hour = 20 , wage=0 , emphrs=0;
 
-		if (empcheck == 1)
+		if (empcheck == 1)  // FullTime
 			emphrs = 8;
-		else
-   		emphrs = 0;
+		else if (empcheck == 2)  // PartTime
+   		emphrs = 4;
+		else								// Absent
+			emphrs = 0;
 
 			wage = wage_per_hour*emphrs;
-			System.out.println("Employee wage="+wage);
+			System.out.println("Employee wage="+wage); 
 
 	}
 
